ca357034352561be494e99d9bdbfdac437fbc0e4
Didier Moindreau <Didux74@gmail.com>
2020-01-31 16:56:01 +0100

    Refactoring DatabaseAbstractLayer.php

--- src/Database/DatabaseAbstractLayer.php
+++ src/Database/DatabaseAbstractLayer.php
@@ -2,11 +2,15 @@
 
 namespace Entity\Database;
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Logging\EchoSQLLogger;
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\ORM\ORMException;
 use Doctrine\ORM\Tools\Setup;
 
@@ -17,54 +21,61 @@ use Doctrine\ORM\Tools\Setup;
  */
 class DatabaseAbstractLayer
 {// Todo refactor in DatabaseDoctrineLayer
-  private string $entitiesPath='/';
-  private $entityManager=null;
-  private $cache = null;
+    private string $entitiesPath = '/';
+    private $entityManager = null;
+    private $cache = null;
     /**
      * @var DriverInterface
      */
     private DriverInterface $driver;
     /**
-     * @var \Doctrine\ORM\Configuration
+     * @var Configuration
      */
-    private \Doctrine\ORM\Configuration $config;
+    private Configuration $config;
 
-    function __construct(DriverInterface $driver, $entitiesPath ,$enableLogger=false)
-  {
-    $isDevMode = true;
-    $this->driver = $driver;
-    $this->entitiesPath = $entitiesPath;
-    $this->cache = $cache = new \Doctrine\Common\Cache\ArrayCache;
-    $this->config =   Setup::createConfiguration($isDevMode);
-    $annotationDriver = new AnnotationDriver(new AnnotationReader(), $this->entitiesPath);
-    // registering noop annotation autoloader - allow all annotations by default
-    AnnotationRegistry::registerLoader('class_exists');
-    $this->config->setMetadataDriverImpl($annotationDriver);
-    /* = Setup::createAnnotationMetadataConfiguration(
-      array(__DIR__.$this->entitiesPath), $isDevMode);*/
-
-    $this->config->setMetadataCacheImpl($cache);
-    $this->config->setQueryCacheImpl($cache);
-    if($enableLogger)
+    function __construct(DriverInterface $driver, $entitiesPath, $enableLogger = true)
     {
-      $logger = new EchoSQLLogger;
-      $this->config->setSQLLogger($logger);
+        $isDevMode = true;
+
+        $this->driver = $driver;
+        $this->entitiesPath = $entitiesPath;
+
+        $this->cache = $cache = new ArrayCache;
+
+        $this->config = Setup::createConfiguration($isDevMode);
+        $annotationDriver = new AnnotationDriver(new AnnotationReader(), $this->entitiesPath);
+        // registering noop annotation autoloader - allow all annotations by default
+        AnnotationRegistry::registerLoader('class_exists');
+        $this->config->setMetadataDriverImpl($annotationDriver);
+        $this->config->setMetadataCacheImpl($cache);
+        $this->config->setQueryCacheImpl($cache);
+
+        if ($enableLogger) {
+            $logger = new EchoSQLLogger;
+            $this->config->setSQLLogger($logger);
+        }
+
+
+        try {
+            $conn = DriverManager::getConnection(
+                [
+                    'driver' => 'pdo_' . $driver->getAttribute(\PDO::ATTR_DRIVER_NAME),
+                    'pdo' => $driver,
+                ]
+            );
+            $this->entityManager = EntityManager::create($conn, $this->config);
+        } catch (ORMException $e) {
+            var_dump($e->getMessage());
+            var_dump($e->getPrevious()->getMessage() . ' ' . __FILE__);
+        } catch (DBALException $e) {
+            var_dump($e->getMessage());
+            var_dump($e->getPrevious()->getMessage() . ' ' . __FILE__);
+        }
     }
-      $conn = \Doctrine\DBAL\DriverManager::getConnection(
-          [
-              'driver' => 'pdo_' . $driver->getAttribute(\PDO::ATTR_DRIVER_NAME),
-              'pdo' => $driver,
-          ],);
-      try {
-          $this->entityManager = EntityManager::create($conn, $this->config);
-      } catch (ORMException $e) {
-          var_dump($e->getMessage());
-      }
-  }
 
 
-  public function getEntityManager()
-  {
-    return $this->entityManager;
-  }
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
 }
