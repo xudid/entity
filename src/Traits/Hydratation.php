c9ba8a2987cad1d306a98593d4e5d66a434947ef
Didier Moindreau <dmoindreauu@gmail.com>
2020-05-09 11:39:17 +0200

    Trait Hydratation.php

---
+++ src/Traits/Hydratation.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Entity\Traits;
+
+
+trait Hydratation
+{
+    public static function hydrate(array $datas)
+    {
+        $object = new static();
+        $methods = get_class_methods(__CLASS__);
+        $keys = array_keys($datas);
+        foreach ($keys as $key) {
+            $key = strtolower($key);
+            $setter = 'set'.ucfirst($key);
+            $value = $datas[$key];
+            if (in_array($setter, $methods)) {
+                $object->$setter($value);
+            }
+        }
+        return $object;
+    }
+}
