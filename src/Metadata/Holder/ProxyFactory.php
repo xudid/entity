0b7899d7b940cec4bd237b70c042436a0ca8f844
Didier Moindreau <dmoindreau@gmail.com>
2020-11-30 20:47:02 +0100

    Refactor generated code

--- src/Metadata/Holder/ProxyFactory.php
+++ src/Metadata/Holder/ProxyFactory.php
@@ -4,28 +4,29 @@
 namespace Entity\Metadata\Holder;
 
 
-use ReflectionClass;
+use Entity\Database\LazyLoader;
 use ReflectionObject;
+use ReflectionClass;
 
 class ProxyFactory
 {
     public function createProxy($object)
     {
-        $r = new ReflectionObject($object);
-        $namespace = $r->getNamespaceName();
-        $objectShortClassName = $r->getShortName();
+        $objectRelfection = new ReflectionObject($object);
+        $namespace = $objectRelfection->getNamespaceName();
+        $objectShortClassName = $objectRelfection->getShortName();
         $shortClassName = $objectShortClassName . 'Proxy';
-        $className = $r->getName() . 'Proxy';
+        $className = $objectRelfection->getName() . 'Proxy';
         $fileClassName = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-         $this->root = dirname($_SERVER['DOCUMENT_ROOT']);
-        $fileName = $this->root . DIRECTORY_SEPARATOR .  'classes' . DIRECTORY_SEPARATOR . $fileClassName . '.php';
+        $fileName = dirname($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR .  'classes' . DIRECTORY_SEPARATOR . $fileClassName . '.php';
         if (!file_exists($fileName)) {
-           $this->generateProxyCLassFile($namespace,$objectShortClassName, $shortClassName, $fileName);
+            $code = $this->generateProxyCode($namespace,$objectShortClassName, $shortClassName);
+            $this->generateProxyClassFile($code, $fileName);
         }
         require_once $fileName;
         try {
-            $r = new ReflectionClass($className);
-            $proxy =  $r->newInstance($object);
+            $proxyRelfection = new ReflectionClass($className);
+            $proxy =  $proxyRelfection->newInstance($object);
             return $proxy;
         } catch (\Exception $exception)
         {
@@ -33,62 +34,105 @@ class ProxyFactory
         }
     }
 
-    private function generateProxyCLassFile($namespace, $objectShortClassName, $shortClassName, $filename)
+    private function generateProxyCode(string $namespace, string $objectShortClassName, string $shortClassName) : string
     {
         $code = "<?php
                 namespace $namespace;
-                class $shortClassName extends $objectShortClassName{" .'
+                
+                 use Entity\Database\LazyLoader;
+                 use Serializable;
+                
+                class $shortClassName extends $objectShortClassName implements Serializable
+                {" .'
                  
                     private  $wrapped;
-                    public function __construct($object)
+                    private $propertyLoaders = [];
+                    private $propertyLoaded = [];
+                    public function __construct($object = null)
                     {
-                        $this->wrapped = $object;
-                        unset($this->id);
-                    }
-                    
-                   
-                    public function __call($name, $args) 
+                        if ($object) {
+                            $this->wrapped = $object;
+                            $this->cleanProperties();
+                        }
+                    }';
+        $code .= 'public function __get($name)
                     {
-                        $args=implode(",",$args);
-                        return $this->wrapped->$name($args);
-                    }
-                    
-                    public static function __callStatic($name, $args)
-                    {
-                        return self::$wrapped->$name($args);
-                    }
-                    
-                    public function __get($name)
-                    {
+                        if (!array_key_exists($name, $this->propertyLoaded)
+                            && array_key_exists($name, $this->propertyLoaders)) {
+                            $loader = $this->propertyLoaders[$name];
+                            if ($loader) {
+                                $value = $loader();
+                                $setter = "set" . ucfirst($name);
+                                $this->wrapped->$setter($value);
+                                $this->propertyLoaded[$name] = true;
+                                $method = "get".ucfirst($name);
+                                return $this->wrapped->$method();
+                            }
+                        }
                         $method = "get".ucfirst($name);
                         return $this->wrapped->$method();
-                    }
-                    
-                    public function __set($name, $value)
+                    }';
+
+        $code .= 'public function __set($name, $value)
                     {
-                        $method = "set" . ucfirst($name);
-                        $this->wrapped->$method($value);
-                        return $this;
-                    }
+                        if ($value instanceof LazyLoader) {
+                            $this->propertyLoaders[$name] = $value;
+                            return $this;
+                        } else {
+                            $setter = "set" . ucfirst($name);
+                            $this->wrapped->$setter($value);
+                            return $this;
+                        }
+                    }';
+        $code .= 'public function serialize()
+                    {
+                        foreach ($this->propertyLoaders as $propertyName => $propertyLoader) {
+                            $value = $propertyLoader();
+                            $setter = "set" . ucfirst($propertyName);
+                            $this->wrapped->$setter($value);
+                            $this->propertyLoaded[$propertyName] = true;
+                        }
+                        return serialize([
+                            "wrapped" => $this->wrapped,
+                            "propertyLoaded" => $this->propertyLoaded,
+                        ]);
+                    }';
+        $code .= 'public function unserialize($serialized)
+                {
+                    $data = unserialize($serialized);
+                    $this->cleanProperties();
+                    $this->wrapped = $data["wrapped"];
+                    $this->propertyLoaded = $data["propertyLoaded"];
                 }';
+        $code .= 'protected function cleanProperties()
+                    {
+                        $selfReflection = new \ReflectionClass(parent::getClass());
+                        $selfProperties = $selfReflection->getProperties();
+                        foreach ($selfProperties as $selfProperty) {
+                            $propertyName = $selfProperty->getName();
+                            if ($selfProperty->isPrivate()) {
+                                throw new \Exception("Proxied object must have protected properties because proxy inherits of his class");
+                            }
+                            if ($propertyName != "wrapped") {
+                                unset($this->$propertyName);
+                            }
+                        }
+                    }';
+        $code .= '
+               }';
+        dump($code);
+        return $code;
+    }
+
+    private function generateProxyClassFile($code, $filename)
+    {
         $this->file_force_contents($filename ,$code);
     }
 
-    private function file_force_contents($dir, $contents){
-        $parts = explode(DIRECTORY_SEPARATOR, $dir);
-        $file = array_pop($parts);
-        $dir = '';
-        $start = 0;
-        if (PHP_OS_FAMILY == 'Windows') {
-            $dir = $parts[$start];
-            $start = 1;
+    private function file_force_contents($fileFullPath, $contents){
+        if (!is_dir(dirname($fileFullPath))) {
+            mkdir(dirname($fileFullPath), 0777,true);
         }
-        for($i = $start;$i < count($parts) ;$i++) {
-            $part = $parts[$i];
-            if(!is_dir($dir .= DIRECTORY_SEPARATOR ."$part")) {
-                mkdir($dir);
-            }
-        }
-        file_put_contents($dir . DIRECTORY_SEPARATOR . $file, $contents);
+        file_put_contents($fileFullPath, $contents);
     }
 }
